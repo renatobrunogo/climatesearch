9fb847b0a0118909576958283d89a7692df89a08
renatobrunogo <contato.renatobruno@gmail.com>
2024-04-14 22:12:10 -0300

    climatesearchgitrepository

---
+++ scripts.js
@@ -0,0 +1,24 @@
+
+
+const key = "4a78c2e634f80be066c1026e2e911ef4"
+
+function colocarDadosNaTela(dados) {
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Clima em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src=`https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}
+
+async function buscarCidade(cidade) {
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(response => response.json())
+
+    colocarDadosNaTela(dados)
+}
+
+function cliqueNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)
+}
